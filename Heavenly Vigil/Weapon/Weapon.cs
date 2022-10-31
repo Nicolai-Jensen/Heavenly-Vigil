7fd841da168dddc90cd44a82d593f8c7a72b16ad
MarcusN <MarcusNN@outlook.dk>
2022-10-31 12:58:37 +0100

    Setup for prject
    Added various classes, fields, methods osv.

---
+++ Heavenly Vigil/Weapon/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heavenly_Vigil
+{
+    internal class Weapon : GameObject
+    {
+    }
+}
