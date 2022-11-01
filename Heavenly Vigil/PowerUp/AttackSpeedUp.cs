46a3f2cd096ea6539147c392316b64d81a6e0686
BrokenModem <larsespenhain@hotmail.com>
2022-11-01 09:28:32 +0100

    Fields n stuff
    Har smidt Methods, og Fields op

--- Heavenly Vigil/PowerUp/AttackSpeedUp.cs
+++ Heavenly Vigil/PowerUp/AttackSpeedUp.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,27 @@ namespace Heavenly_Vigil
 {
     internal class AttackSpeedUp : PowerUp
     {
+        //Fields
+        public int attackSpdAmp;
+        //Properties
+        //Constructors
+        //Methods
+        public override void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, GraphicsDeviceManager _graphics)
+        {
+
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+        public override void AddValue(GameObject player)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Heavenly Vigil/PowerUp/PowerUp.cs
+++ Heavenly Vigil/PowerUp/PowerUp.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace Heavenly_Vigil
 {
-    internal class PowerUp : GameObject
+    public abstract class PowerUp : GameObject
     {
+        //Fields
+        protected int valueAmp;
+        //Properties
+        //Constructors
+        //Methods
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+        public abstract void Draw(SpriteBatch spriteBatch);
+        public abstract void AddValue(GameObject player);
     }
 }
