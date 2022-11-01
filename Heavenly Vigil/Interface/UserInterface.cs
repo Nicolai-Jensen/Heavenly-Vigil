2aedba1e91619b1d6d8d3e3d5329ab9bc37d18d9
MarcusN <MarcusNN@outlook.dk>
2022-11-01 09:30:31 +0100

    UserInterface begun
    Added minor functionality to UserInterface.

--- Heavenly Vigil/Interface/UserInterface.cs
+++ Heavenly Vigil/Interface/UserInterface.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,57 @@ namespace Heavenly_Vigil
 {
     internal class UserInterface : GameObject
     {
+        //-----FIELDS-----
+
+        private int health;
+        private Vector2 outlinePosition;
+        private Vector2 healthBarPosition;
+        private Texture2D outlineSprite;
+        private Texture2D healthBarSprite;
+
+        private Rectangle outlineR;
+        private Rectangle healthBarR;
+
+        //-----PROPERTIES-----
+
+
+
+        //-----CONSTRUCTORS-----
+
+        public UserInterface()
+        {
+            health = 100;
+            outlinePosition = new Vector2(0, 0);
+            healthBarPosition = new Vector2(10, 5);
+
+            outlineR.Width = 120;
+            outlineR.Height = 20;
+            healthBarR.Width = 100;
+            healthBarR.Height = 10;
+
+        }
+
+        //-----METHODS-----
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            outlineSprite = content.Load<Texture2D>("");
+            healthBarSprite = content.Load<Texture2D>("");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            AdjustHealthBar();
+        }
+
+        private void AdjustHealthBar()
+        {
+            healthBarR.Width = health;
+        }
     }
 }
